b3a2d3f3c623df84f7bdf0781901ebf1230ed630
dimakarp1996 <dimakarp1996@yandex.ru>
2019-01-15 21:45:21 +0300

    Скрипт для снятия фото

---
+++ ImageCapture.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 21:35:47 2019
+
+@author: robocup
+"""
+import qi
+from PIL import Image
+IP='192.168.1.13'
+PORT=9559
+resolution = 2
+CameraIndex=1   # VGA
+colorSpace = 11 
+ses = qi.Session()
+ses.connect(IP)
+YAW,PITCH=0,0
+video= ses.service('ALVideoDevice')
+motionProxy  = ses.service('ALMotion')
+postureProxy = ses.service('ALRobotPosture')
+def get_photo(pitch, yaw, speed,
+              cameraIndex=1,
+              image_name=None,
+              save_image=True
+              ):
+    '''
+    pitch - vertical angle(in RAD, from -0.67 to 0.51 )
+    yaw - horisontal angle(in RAD, from -2.08 to 2.08)
+    speed - speed of placing angles to this place
+    cameraIndex  -index of camera ( 0 is top camera, 1 is bottom camera)
+    save_image : whether we save image or not
+    image_name - name used for saving(if not specified, default name is generated)
+    '''
+    if not image_name:
+        image_name='IMAGE_'+str(yaw)+'_'+str(pitch)
+    motionProxy.setAngles(["HeadPitch","HeadYaw"],[pitch,yaw],speed)#
+    videoClient = video.subscribeCamera(
+        "python_client", CameraIndex, resolution, colorSpace, 5)
+    naoImage = video.getImageRemote(videoClient)
+    video.unsubscribe(videoClient)
+        
+    imageWidth = naoImage[0]
+    imageHeight = naoImage[1]
+    array = naoImage[6]
+    im = Image.frombytes("RGB", (imageWidth, imageHeight), str(array))
+    if save_image:
+        im.save(image_name)
+    return im
