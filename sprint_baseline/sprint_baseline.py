0ea57f57c787946266ee444e6528a0eab3821a9c
dimakarp1996 <dimakarp1996@yandex.ru>
2019-02-21 21:42:49 +0300

    Add files via upload

--- sprint_baseline/sprint_baseline.py
+++ sprint_baseline/sprint_baseline.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 21 20:56:51 2019
+
+@author: asus
+"""
+
 # -*- encoding: UTF-8 -*- 
 
 '''Walk: Small example to make Nao walk'''
@@ -13,14 +21,92 @@ import vision_definitions
 import cv2
 import numpy as np
 from PIL import Image
-
+TURNBACK_FLAG=False
 #from enum import Enum
 
 marker_color_min = (0, 240, 135)
 marker_color_max = (20, 255, 205)
 
-WIND_X = 640
-WIND_Y = 480
+X_DIM = 640
+Y_DIM = 480
+class Line():
+    def __init__(self,k, b, x_min,x_max):
+        self.k,self.b=k,b
+        self.x_min,self.x_max = x_min,x_max
+        y_extrs = [k*x_min+b,k*x_max+b]
+        self.y_min,self.y_max = min(y_extrs),max(y_extrs)
+def getLines(image):
+    pass
+def processLines(lines, X_checkpoint=X_DIM/2,Y_checkpoint=Y_DIM/2):
+    left_xmax,right_xmin,center_ymin=0,9999,9999
+    left_line,right_line,horisontal_line=None,None,None
+    for line in lines:
+        if (line.xmax<X_checkpoint and line.xmax>left_xmax):
+            left_line,left_xmax=line,  line.xmax
+        elif (line.xmin>X_checkpoint and line.xmin<right_xmin):
+            right_line,right_xmin = line, line.xmin
+        else:
+            center_y=(line.y_min + line.y_max)/2
+            if center_y<center_ymin:
+                horisontal_line,center_ymin=line,center_y
+	return [left_line,horisontal_line,right_line]
+
+def getChessboardCentre(image, pattern_size):#If we use chessboard
+    '''
+    image is np.array, pattern_size istuple
+    '''
+    ans=None
+    for i in range(3, pattern_size.shape[0]+1):
+        for j in range(3,pattern_size.shape[1]+1):
+            found, corners = cv2.findChessboardCorners(image,(i,j),None)
+            if found:
+                ans = corners.sum(0)/len(corners)
+    return ans
+                    
+
+def ObjectCondition(center_y, allowed_gamma=100):
+    if center_y < Y_DIM/2 - allowed_gamma:
+        return -1
+    elif center_y>Y_DIM/2 + allowed_gamma:
+        return 1
+    else:
+        return 0
+def LineCondition(left_line, right_line):
+     if left_line is None and right_line is not None:
+         return 1
+     if right_line is None and left_line is not None:
+        return -1
+     return 0         
+     
+def BackCondition(line, center):
+    if line is None or line.y_min>center.y:
+        return 1
+    return 0
+def shouldTurn(image, allowed_gamma=100, X_checkpoint=X_DIM//2,Y_checkpoint=Y_DIM//2):
+    global TURNBACK_FLAG
+    raw_lines = getLines(image)#list of objects class Line
+    left_line,right_line,horisontal_line = processLines(raw_lines, X_checkpoint,Y_checkpoint)
+    center = getChessboardCentre(image)#maybe other function and other object instead of chessboard
+    back_condition = BackCondition(horisontal_line,center)
+    line_condition = LineCondition(left_line,right_line)
+    object_condition = ObjectCondition(center[1],allowed_gamma)
+    if back_condition:
+        TURNBACK_FLAG=True
+    if abs(object_condition)>1:
+         return object_condition
+    if abs(line_condition)>1:
+         return line_condition
+    return 0
+
+def GetNewAngle(current_angle,turn_command, delta):
+    '''
+    turn_command - result of ShouldTurn function
+    '''
+    return current_angle + turn_command * delta
+
+
+
+
 
 def StiffnessOn(proxy):
     # We use the "Body" name to signify the collection of all joints
