3f3b8705154e951aea23dc15ee25c7315967517c
dimakarp1996 <dimakarp1996@yandex.ru>
2019-02-22 22:23:17 +0300

    Add files via upload

--- sprint_baseline/sprint_baseline.py
+++ sprint_baseline/sprint_baseline.py
@@ -1,55 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 22 14:05:12 2019
 
-@author: user
-"""
-
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 21 20:56:51 2019
-@author: asus
-"""
-
-# -*- encoding: UTF-8 -*- 
-
-'''Walk: Small example to make Nao walk'''
 import sys
-import motion
 import time
-from naoqi import ALProxy
 import qi
 import argparse
 import sys
 import time
-import vision_definitions
 import cv2
 import numpy as np
 from PIL import Image
 TURNBACK_FLAG=False
 STOP_FLAG = False
 #from enum import Enum
-
+#img image rename
 marker_color_min = (0, 240, 135)#Min color of red box
 marker_color_max = (20, 255, 205)#Max color of red box
-pattern_size = (8,6)#Pattern size of chessboard
-X_DIM = 640
-Y_DIM = 480
-class Line():
-    def __init__(self,k, b, x_min,x_max):
-        self.k,self.b=k,b
-        self.x_min,self.x_max = x_min,x_max
-        y_extrs = [k*x_min+b,k*x_max+b]
-        self.y_min,self.y_max = min(y_extrs),max(y_extrs)
-    def y(self,x):
-        return self.k*x+self.b
-from line_detection import getLines
-def processLines(lines, X_checkpoint=X_DIM/2,Y_checkpoint=Y_DIM/2):
+pattern_size = (3,3)#Pattern size of chessboard
+
+from line_detect import getLines,Line,X_DIM,Y_DIM
+def processLines(raw_lines, X_checkpoint=X_DIM/2,Y_checkpoint=Y_DIM/2):
     left_xmax,right_xmin,center_ymin=0,9999,9999
     left_line,right_line,horisontal_line=None,None,None
-    smallest_k = min([line.k for line in lines if line.k<0])
-    largest_k = max([line.k for line in lines if line.k>0])
+    smallest_k = min([line.k for line in raw_lines if line.k<0]+[9999999])
+    largest_k = max([line.k for line in raw_lines if line.k>0]+[-9999999])
     n=2
     def canBeLeft(line):
         cond1 = (
@@ -62,32 +34,33 @@ def processLines(lines, X_checkpoint=X_DIM/2,Y_checkpoint=Y_DIM/2):
         cond2 = (line.k<smallest_k/n)
         return cond1 or cond2
     
-    for line in lines:
+    for line in raw_lines:
         if (canBeLeft(line) and line.xmax>left_xmax):
             left_line,left_xmax=line,  line.xmax
         elif (canBeRight(line) and line.xmin<right_xmin):
             right_line,right_xmin = line, line.xmin
         else:
-            center_y=(line.y_min + line.y_max)/2
+            center_y=(line.ymin + line.ymax)/2
             if center_y<center_ymin:
                 horisontal_line,center_ymin=line,center_y
     return [left_line,horisontal_line,right_line]
 def log(image, lines,thickness=5):
     t = str(time.time())
-    cv2.imwrite('prev_'+t,image)
+    cv2.imwrite('prev_'+t+'.jpg',image)
     colors=[(255,0,0),(0,255,0),(0,0,255)]
     for (line,color) in zip(lines,colors):
         if line is not None:
-            cv2.line(image,(line.xmin,line.y(line.x_min),
-            (line.x_max,line.y(line.x_max)),color,thickness))
-    cv2.imsave(t,image)
+            cv2.line(image,
+            (line.xmin,line.y(line.xmin)),
+            (line.xmax,line.y(line.xmax)),color,thickness)
+    cv2.imwrite(t+'.jpg',image)
 def getChessboardCentre(image, pattern_size=pattern_size):#If we use chessboard
     '''
     image is np.array, pattern_size istuple
     '''
     ans=None
-    for i in range(3, pattern_size.shape[0]+1):
-        for j in range(3,pattern_size.shape[1]+1):
+    for i in range(3, pattern_size[0]+1):
+        for j in range(3,pattern_size[1]+1):
             found, corners = cv2.findChessboardCorners(image,(i,j),None)
             if found:
                 ans = corners.sum(0)/len(corners)
@@ -179,12 +152,12 @@ def make_proxies(robotIP,port=9559, session=False):
     '''
     if session is False, ALProxys are created. 
     Otherwise, connections through qi.Session are created.
-
     '''
     proxy_names = ['ALMotion','MovementGraph','ALRobotPosture',
                    'ALVideoDevice']
     proxy_list=[]
     if not session:
+        from naoqi import ALProxy
         for proxy_name in proxy_names:
             try:
                 proxy_list.append(
@@ -194,20 +167,14 @@ def make_proxies(robotIP,port=9559, session=False):
                 print('Error was: '+str(e))
                 raise Exception()
     elif session:
-        sess = qi.Session()
-        try:
-            sess.connect("tcp://"+robotIP+":"+str(port))
-            for proxy_name in proxy_names:
-                proxy_list.append(sess.service(proxy_name))
-        except:
-            print('Can not connect at ip '+robotIP+' and port '+str(port))
-            raise Exception()
+        return None
+            
     return proxy_list
 def subscribe(video_service, camera_id=0,
               resolution=2, colorSpace=11,fps=5):
     ###Subscribes videoservice to camera
     ###I'm not sure if last parameter really stands for fps - it needs to be checked
-    return video_service.subscribeCamera(camera_id,resolution, colorSpace, fps)
+    return video_service.subscribeCamera('python_client',camera_id,resolution, colorSpace, fps)
 
 def get_photo(video_service,video_client, photo_ind = 0, time_est=True):
     '''
@@ -226,20 +193,33 @@ def finish(postureProxy, video_service,video_client):
     time.sleep(1.0)
     print('finished')
     video_service.unsubscribe(video_client)
-
+time_lag,stand_speed=2,0.5
+robotIP='192.168.1.67'
 def main(robotIP, time_lag = 2, stand_speed= 0.5):
     # Init proxies.
-    motionProxy, customProxy, postureProxy, video_service = make_proxies(robotIP,9559,False)
-    
+    #motionProxy, customProxy, postureProxy, video_service = make_proxies(robotIP,9559,False)
+    proxy_list=[]
+    proxy_names,port=['ALMotion','MovementGraph','ALRobotPosture',
+                   'ALVideoDevice'],9559
+    sess = qi.Session()
+    try:
+        sess.connect("tcp://"+robotIP+":"+str(port))
+        for proxy_name in proxy_names:
+            proxy_list.append(sess.service(proxy_name))
+    except:
+        print('Can not connect at ip '+robotIP+' and port '+str(port))
+        raise Exception()
+    motionProxy, customProxy, postureProxy, video_service = proxy_list
     StiffnessOn(motionProxy)
     motionProxy.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
     postureProxy.goToPosture("StandInit", stand_speed)
+
     
     video_client = subscribe(video_service, 0)
     prev_time = 0
     n_times = 5    
     print ("starting")
-    for i in range(len(n_times)):
+    for i in range((n_times)):
         this_time = time.time()
         if this_time>prev_time+time_lag:
             img = get_photo(video_service, video_client, photo_ind=i)
